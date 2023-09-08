fbb2e9220c0c8cb9a32e8690ee0f78dc085c975f
Tinu  kumar <tinu01edu@gmail.com>
2023-09-08 13:42:30 +0530

    changed

--- Controller/usercontroller.js
+++ Controller/usercontroller.js
@@ -130,7 +130,7 @@ export const signupuser=async(req,res)=>{
 
         const subject='your verification link';
 
-        const text=`Please verify your email by clicking on the following link: http://localhost:3000/verify/${verificationtoken}`;
+        const text=`Please verify your email by clicking on the following link: http://full-auth-82oj.onrender.com/verify/${verificationtoken}`;
             console.log(email , subject , text)
             
         if(sendEmail(email , subject , text))
