ebdbfb416dbd2e1440697322253943a67d418000
Tinu  kumar <tinu01edu@gmail.com>
2023-09-25 20:25:44 +0530

    first commit

--- Router/route.js
+++ Router/route.js
@@ -2,7 +2,16 @@ import  express  from "express";
 import { signupuser , verifytokenbyuser, loginuser , forgotpassword ,Otpverification , Changepassword , logoutuser} from "../Controller/usercontroller.js";
 import { fetchallemail  , deletemail} from "../Controller/admin.js";
 
-
+const isAdminAPIAuthorized = (req, res, next) => {
+    // Assuming you have user information stored in req.user after authentication.
+    if (req.user && req.user.role === 'admin' && req.user.permissions.includes('access_admin_api')) {
+      // User has the necessary role and permission.
+      next(); // Allow access to /admin/api.
+    } else {
+      // User doesn't have the required access.
+      res.status(403).json({ error: 'Access Denied' });
+    }
+  };
 
 
 const router=express.Router();
@@ -22,6 +31,6 @@ router.post('/otpverification' , Otpverification)
 router.post('/changepassword' , Changepassword)
 
 router.put('/verify/:token' , verifytokenbyuser )
-router.get('/admin/api' , fetchallemail)
+router.get('/admin/api' ,isAdminAPIAuthorized , fetchallemail)
 router.post('/admin/email/delete' ,deletemail)
 export default router;
